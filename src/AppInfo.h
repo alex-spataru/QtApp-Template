2b1c8c4a118accbd7aeb8d3da1b190d6f173a1dc
Alex Spataru <alex_spataru@outlook.com>
2021-01-19 17:02:35 -0500

    Rename Qt.svg to icon.svg

--- src/AppInfo.h
+++ src/AppInfo.h
@@ -8,7 +8,7 @@
 #define APP_VERSION     "1.0.0"
 #define APP_DEVELOPER   "Awesome Developer"
 #define APP_NAME        "Qt App"
-#define APP_ICON        ":/images/Qt.svg"
+#define APP_ICON        ":/images/icon.svg"
 #define APP_SUPPORT_URL "https://github.com/alex-spataru/QtApp-Template"
 #define APP_UPDATER_URL "https://raw.githubusercontent.com/alex-spataru/QtApp-Template/master/deploy/updates.json"
 #define LOG_FORMAT      "[%{time}] %{message:-72} [%{TypeOne}] [%{function}]\n"
